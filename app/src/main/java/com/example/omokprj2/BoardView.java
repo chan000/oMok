91460feffa0e53ffcc588e9d979b33dd52507769
tmddbs8115 <tmddbs8115@gmail.com>
2021-05-10 17:27:18 +0900

    no message

--- app/src/main/java/com/example/omokprj2/BoardView.java
+++ app/src/main/java/com/example/omokprj2/BoardView.java
@@ -20,10 +20,23 @@ public class BoardView extends View implements View.OnTouchListener {
 
     private Paint paint;
     private Bitmap grid;
-    private Bitmap cursor, blueStone;
+    private Bitmap cursor, blueStone, redStone;
+
+    public final static int NONE = 100;
+    public final static int CROSS = 0;
+    public final static int NOUGHT = 1;
+
+
+    protected int player;         // The player whose move is next 다음 차례가 될 선수
+    protected int totalLines;     // The number of empty lines left 남은 빈 줄의 수
+    protected boolean isWon;      // Set if one of the players has won 플레이어중 한명이 이겼는지 설정
+    protected int[][][][] line;   // Number of pieces in each of all possible lines 가능한 모든 라인의 조각 수     //금수를 추가할 수 있어
+    protected int[][][] value;    // Value of each square for each player 각 플레이어의 제곱값
+
 
 
     private int curX, curY;
+    private int prevX, prevY;
 
     private double scaleRate;
 
@@ -33,7 +46,32 @@ public class BoardView extends View implements View.OnTouchListener {
         super(context, attrs);
         cursor = BitmapFactory.decodeResource(getResources(), R.drawable.cursor);
         blueStone = BitmapFactory.decodeResource(getResources(), R.drawable.blue_stone);
+        redStone = BitmapFactory.decodeResource(getResources(), R.drawable.red_stone);
         setOnTouchListener(this);
+    }
+
+
+
+    public void resetGame() {
+        board = new int[boardSize][boardSize];
+        value = new int[boardSize][boardSize][2];
+        line = new int[4][boardSize][boardSize][2];
+
+        for (int i = 0; i < boardSize; ++i) {
+            for (int j = 0; j < boardSize; ++j) {      // Clear tables  비어있는 테이블
+                board[i][j] = NONE;                     //예상- 앞의 두개배열은 테이블의 배열 다음 c는 돌의 statues
+                for (int c = 0; c < 2; ++c) {           //돌의 구분
+                    value[i][j][c] = 0;
+                    for (int d = 0; d < 4; ++d) {       //5개가 연속으로 오는 돌을 확인하는 배열
+                        line[d][i][j][c] = 0;
+                    }
+                }
+            }
+        }
+        player = CROSS;      // cross starts
+        // Total number of lines
+        totalLines = 2 * 2 * (boardSize * (boardSize - 4) + (boardSize - 4) * (boardSize - 4));
+        isWon = false;
     }
 
     private static Bitmap scaleBitmap(Bitmap original, double rate) {
@@ -59,6 +97,8 @@ public class BoardView extends View implements View.OnTouchListener {
         if ( scaleRate < 1 ) {
 
             cursor = scaleBitmap(cursor);
+            blueStone = scaleBitmap(blueStone);
+            redStone = scaleBitmap(redStone);
         }
     }
 
@@ -107,6 +147,39 @@ public class BoardView extends View implements View.OnTouchListener {
         canvas.drawBitmap(cursor, posX, posY, paint);
 
     }
+    protected void drawStones(Canvas canvas){
+        resetGame();
+        int cellSize = canvas.getHeight() / boardSize;
+        int offset = (cellSize - blueStone.getWidth()) / 2;
+        for (int i = 0; i < boardSize; i++){
+            float x = cellSize*i + offset;
+            for (int j = 0; j < boardSize; j++){
+                if ( board[i][j] == NONE ) {         //2차 행렬안에 돌이 없으면 실행한다.
+                    continue;
+                }else{
+                 Bitmap bmp =blueStone;
+                float y = cellSize*j + offset;
+                canvas.drawBitmap(bmp, x, y, paint);
+                }
+            }
+        }
+    }
+    private void makeMove(int x, int y){
+        board[x][y] = player;
+    }
+    public void playerMove(int x, int y){
+        if (board[x][y] == NONE){
+           makeMove(x,y);
+        }
+    }
+    public void playerMove1(){
+        playerMove(curX,curY);
+    }
+
+    private int getOpponent() {
+        return player == CROSS ? NOUGHT : CROSS;
+    }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -117,6 +190,7 @@ public class BoardView extends View implements View.OnTouchListener {
         }
         canvas.drawColor(Color.TRANSPARENT);
         canvas.drawBitmap(grid, 0, 0, paint);
+        drawStones(canvas);
        showCursor(canvas);
 
     }
@@ -137,6 +211,12 @@ public class BoardView extends View implements View.OnTouchListener {
             invalidate();
     }
 
+    public void setBoard(int[][] board, int x, int y) {
+        this.prevX = x;
+        this.prevY = y;
+        this.board = board;
+        invalidate();
+    }
 
 
 
--- app/src/main/java/com/example/omokprj2/MainActivity.java
+++ app/src/main/java/com/example/omokprj2/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.omokprj2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnput;
+    protected BoardView logic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnput = (ImageButton)findViewById(R.id.btnput);
+
+        btnput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logic.playerMove1();
+            }
+        });
     }
 }
